7c75aeb7d209eca6dde6e869968dbade1b04f0e2
BasvBeem <b.vanbeem@student.fontys.nl>
2023-09-14 16:14:25 +0200

    dsl

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,3 +1,23 @@
+import streamlit as st
+import matplotlib.pyplot as plt
 import numpy as np
-a = np.array([5,6,7])
-print(a)
+def grid():
+    xgrid = [1,1,1,0,3,2,2,2,2,0,3]
+    ygrid = [3,0,1,1,1,1,3,0,2,2,2]
+    arr = np.random.normal(1, 1, size=100)
+    fig, ax = plt.subplots()
+    ax.plot(xgrid,ygrid)
+    return fig
+def cross(x,y):
+    xcross = [x-0.45,x+0.45,x,x+0.45,x-0.45]
+    ycross = [y+0.45,y-0.45,y,y+0.45,y-0.45]
+    arr = np.random.normal(1, 1, size=100)
+    fig, ax = plt.subplots()
+    ax.plot(xcross,ycross)
+    return fig
+figg = grid()
+figc = cross(1.5,1.5)
+st.pyplot(figg)
+st.pyplot(figc)
+
+
